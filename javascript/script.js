9a2d7f4b82833731f0808f8f9c2dbd639a2e3513
Shreyas Salian <shreyassalian925@gmail.com>
2021-07-26 10:07:51 +0530

    Basic functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,84 @@
+const operation = {
+  firstValue: null,
+  currentOperation: null,
+  startOfSecond: false
+}
+
+const displayBar = document.querySelector('input');
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach( item => { item.addEventListener('click', selectNumber);
+});
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(item => {
+    item.addEventListener('click', selectOperation);
+});
+
+const equals = document.getElementById('equals');
+equals.addEventListener('click', performOperation);
+
+function selectNumber(e) {
+  emptyDisplay();
+  displayNumber(this.firstChild.textContent);
+}
+
+function isSecondValue() {
+  return operation.startOfSecond;
+}
+
+function emptyDisplay() {
+  if (displayBar.value === '0' || isSecondValue()) displayBar.value = '';
+  else if (displayBar.value.includes('ERROR')) displayBar.value = '';
+
+  if (operation.startOfSecond) operation.startOfSecond = false;
+}
+
+function getDisplayValue() {
+  return +displayBar.value;
+}
+
+function displayNumber(number) {
+  displayBar.value += number;
+}
+
+function selectOperation(e) {
+  operation.firstValue = getDisplayValue();
+  operation.currentOperation = this.firstChild.textContent;
+
+  operation.startOfSecond = true;
+}
+
+function performOperation() {
+  const solution = operate(
+    operation.currentOperation,
+    operation.firstValue,
+    getDisplayValue()
+  );
+
+  displayBar.value = isNaN(solution) ? 'ERROR: NaN': solution;
+}
+
 function operate(opearator, num1, num2) {
+  let solution;
 
   switch (opearator) {
     case '+':
-      alert(add(num1, num2));
+      solution = add(num1, num2);
       break;
     case '-':
-      alert(substract(num1, num2));
+      solution = substract(num1, num2);
       break;
-    case '*':
-      alert(multiply(num1, num2));
+    case 'x':
+      solution = multiply(num1, num2);
       break;
     case '/':
-      alert(divide(num1, num2));
+      solution = divide(num1, num2);
       break;
-    default:;
+    default: solution = 'ERROR!';
   }
+
+  return solution;
 }
 
 
